69c6fe190e069cbb8b709f92dad254c272faa607
danglecodhq <dang.le@codehq.nz>
2025-11-14 20:05:41 +0700

    make the menu items centered vertically, the last menu items stick to the bottom

--- src/dashboard/sidebar/SidebarItems.tsx
+++ src/dashboard/sidebar/SidebarItems.tsx
@@ -10,20 +10,42 @@ const style = {
 export function SidebarItems() {
   const { pathname } = useLocation();
 
+  const main = data.slice(0, -1);
+  const last = data[data.length - 1];
+
   return (
-    <ul className="flex grow flex-col">
-      {data.map((item, index) => (
-        <li key={item.title} className={index === data.length - 1 ? 'mt-auto' : ''}>
-          <Link to={item.link}>
+    <div className="flex grow flex-col">
+      {/* Centered main items */}
+      <div className="flex grow flex-col items-center justify-center">
+        {main.map(item => (
+          <div key={item.title} className="w-full">
+            <Link to={item.link}>
+              <span
+                className={`${style.link} ${
+                  item.link === pathname ? style.active : style.inactive
+                }`}
+              >
+                <span className="text-xl">{item.icon}</span>
+                <span className="mt-2 text-center text-xs">{item.title}</span>
+              </span>
+            </Link>
+          </div>
+        ))}
+      </div>
+
+      {/* Bottom settings item */}
+      {last && (
+        <div className="w-full">
+          <Link to={last.link}>
             <span
-              className={`${style.link} ${item.link === pathname ? style.active : style.inactive}`}
+              className={`${style.link} ${last.link === pathname ? style.active : style.inactive}`}
             >
-              <span className="text-xl">{item.icon}</span>
-              <span className="mt-2 text-center text-xs">{item.title}</span>
+              <span className="text-xl">{last.icon}</span>
+              <span className="mt-2 text-center text-xs">{last.title}</span>
             </span>
           </Link>
-        </li>
-      ))}
-    </ul>
+        </div>
+      )}
+    </div>
   );
 }
