84fbcbb899cd98bc773e49b40b77f12216a4c4fa
danglecodhq <dang.le@codehq.nz>
2025-11-16 07:36:53 +0700

    Replace insight icon

--- src/dashboard/sidebar/icons/InsightIcon.tsx
+++ src/dashboard/sidebar/icons/InsightIcon.tsx
@@ -1,5 +1,5 @@
-import { faChartPie } from '@fortawesome/free-solid-svg-icons'; // or faCog for the older icon
+import { faGaugeHigh } from '@fortawesome/free-solid-svg-icons'; // or faCog for the older icon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export function InsightIcon() {
-  return <FontAwesomeIcon icon={faChartPie} />;
+  return <FontAwesomeIcon icon={faGaugeHigh} />;
 }
